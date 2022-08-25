dc10aa261feb3a8ff418f445ba99c6b3a4ae0be0
S-Reo <Csrengin115it@gmail.com>
2022-08-25 20:36:54 +0900

    logの計算無し。そのまま値を使用

--- src/Observation.cpp
+++ src/Observation.cpp
@@ -19,8 +19,13 @@ void Observation::setValues(int left_f,int left_s, int right_s, int right_f)
 	rs = right_s > 0 ? right_s : 1;
 	rf = right_f > 0 ? right_f : 1;
 
-	log_lf = log10((double)lf);
+/*	log_lf = log10((double)lf);
 	log_ls = log10((double)ls);
 	log_rs = log10((double)rs);
 	log_rf = log10((double)rf);
+*/
+	log_lf = (double)lf;
+	log_ls = (double)ls;
+	log_rs = (double)rs;
+	log_rf = (double)rf;
 }
